c6fa64ce38016c61fd297ce78784eae5e82521fe
philipstar1234567 <philip.sandersen@hotmail.com>
2024-09-30 13:19:55 +0200

    Oppdaterer min fil

--- Philip.cpp
+++ Philip.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 #include "Philip.h"
 
-int main() {
+int StartGame() {
+    for (int i = 0; i < 5; i++)
+        cout << rand() << " ";
 
+    return 0;
 }
