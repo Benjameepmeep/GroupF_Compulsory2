16323d5599c1aba4b14d8dd7eefbb87b4861c32c
Benjamin Alexander <146328131+Benjameepmeep@users.noreply.github.com>
2024-10-01 15:51:50 +0200

    Benjamin.cpp

---
+++ Benjamin.cpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <iostream>
+#include "Wilhelm.h"
+
+class Player_Base 
+{
+private:
+	int maxHealth;
+	int currentHealth = maxHealth;
+	int light_attack;
+	int hard_attack;
+
+public:
+	void setHealth(int health) 
+	{
+		maxHealth = health;
+	}
+	void setLAttack(int lightDamage) 
+	{
+		light_attack = lightDamage;
+	}
+	void setHAttack(int hardDamage) 
+	{
+		hard_attack = hardDamage;
+	}
+	void attack(int damage) 
+	{
+		currentHealth -= damage;
+	}
+	void heal(int healing) 
+	{
+		currentHealth += healing;
+	}
+
+	// TODO: Create a string function that can print out different kinds of information. : )
+
+	void print()
+	{
+		// Print what has happened (e.g. "PLayer took 3 damage from Enemy)
+	}
+};
+
+class Enemy: public Player_Base
+{
+
+};
