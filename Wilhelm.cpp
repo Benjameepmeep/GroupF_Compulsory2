10bb9054aa7f54d71f7dfdd735cab711adeecf98
Wilhelm971 <276351@stud.inn.no>
2024-10-01 15:02:00 +0200

    Update Wilhelm.cpp

--- Wilhelm.cpp
+++ Wilhelm.cpp
@@ -1,2 +1,31 @@
 /*Dette er en melding*/
+<<<<<<< Updated upstream
 // Test
+=======
+#include <iostream>
+using namespace std;
+
+
+class Player_Base {
+private:
+	int health;
+	int light_attack;
+	int hard_attack;
+
+public:
+	void setHealth(int h) {
+		health = h;
+	}
+	void setLAttack(int la) {
+		light_attack = la;
+	}
+	void setHAttack(int ha) {
+		hard_attack = ha;
+	}
+
+};
+
+void main() {
+
+}
+>>>>>>> Stashed changes
