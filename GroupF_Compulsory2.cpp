c53f8727d6b9ffc5c6979fc45cba9b71951508eb
KirbyWithACoolHat <276341@stud.inn.no>
2024-10-03 14:21:28 +0200

    Made a function called "requestRestart"

--- GroupF_Compulsory2.cpp
+++ GroupF_Compulsory2.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
 #include <string>
-int main()
+#include "GroupF_Compulsory2.h"
+void requestRestart()
 {
-    //If we make a new gamemode, this is where we ask the user which mode to choose. For now, line 5 does nothing.
-    
-    //This line will call startGame(), which I will not add until that has been developed more.
     std::cout << "Do you want to battle again? (y/n)\n";
-    std::string input;
+    std::string input = "NoInput";
     bool answeredWhetherToTryAgain = false;
-    while (answeredWhetherToTryAgain = false)
+    while (answeredWhetherToTryAgain == false)
     {
-        std::cin >> (input);
-        
+        std::cin >> input;
+
         if (input == "y")
         {
+            main();
             answeredWhetherToTryAgain = true;
         }
         else if (input == "n")
@@ -24,20 +23,13 @@ int main()
         {
             std::cout << "Invalid input.\n";
         }
-
-        //switch (input)
-        //{
-        //case "y": //Note: this causes an error, as it does not recognize y as a string. Will fix on thursday.
-        //    main();
-        //    answeredWhetherToTryAgain = true;
-        //    break;
-        //case "n":
-        //    answeredWhetherToTryAgain = true;
-        //    break;
-        //default:
-        //    std::cout << "Invalid input.\n";
-        //    break;
-        //}
     }
+}
+int main()
+{
+    //If we make a new gamemode, this is where we ask the user which mode to choose. For now, line 5 does nothing.
+    
+    //This line will call startGame(), which I will not add until that has been developed more.
+    requestRestart();
     return 0;
 }
