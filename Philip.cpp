d585b4410c3873a088f9645afa090b1ed0550237
philipstar1234567 <philip.sandersen@hotmail.com>
2024-10-03 15:11:46 +0200

    Random bool

--- Philip.cpp
+++ Philip.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 #include "Philip.h"
 using namespace std;
 
 int StartGame() {
-    for (int i = 0; i < 10; i++)
-    {
-        cout << rand()%2 << endl;
-    }
-    int turn = rand() % 2; //1 = playerturn, 0 = enemyturn
+    srand(static_cast<unsigned int>(time(0)));
+    int turn = rand()%2; //1 = playerturn, 0 = enemyturn
     cout << turn << endl;
     return 0;
 }
