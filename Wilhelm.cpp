5253c4885c6d86a4d4b18ab51ffefc7370196a5b
Wilhelm971 <276351@stud.inn.no>
2024-10-03 13:57:50 +0200

    Update Wilhelm.cpp

--- Wilhelm.cpp
+++ Wilhelm.cpp
@@ -1,48 +1,104 @@
 #include <iostream>
 #include <string>
 #include "Player.h"
-using namespace std;
 
 
 class Player_Base {
-private:
-	int maxHealth = 0;
-	int currentHealth = maxHealth;
-	int light_attack = 0;
-	int hard_attack = 0;
+	private:
+		int health;
+		int light_attack;
+		int critChance;
+		int healingStat;
+		int hard_attack;
+		int damage;
 
-public:
-	void setHealth(int health) {
+	public:
+		void setHealth(int healing) {
+			health = healing;
+			}
 
-		maxHealth = health;
-	}
-	void setLAttack(int lightDamage) {
+		void setHealingStat(int healing) {
+			healingStat = healing;
+			}
 
-		light_attack = lightDamage;
-	}
-	void setHAttack(int hardDamage) {
+		void setLAttack(int lightDamage) {
+			light_attack = lightDamage;
+			}
 
-		hard_attack = hardDamage;
-	}
-	void attack(int damage) {
+		void setHAttack(int hardDamage) {
+			hard_attack = hardDamage;
+			}
 
-		currentHealth -= damage;
+		int getLAttack() {
+			return light_attack;
+			}
 
-	}
-	void heal(int healing) {
+		int getHAttack() {
+			return hard_attack;
+			}
 
-		currentHealth += healing;
+		int getHealth() {
+			return health;
+			}
 
-	}
-	void print(string text) {
-		cout << "string";
-	}
+		int getDamage() {
+			critChance = rand() % 5 + 1;
+
+			switch (critChance) {
+			case 5:
+				damage = hard_attack;
+				break;
+
+			default:
+				damage = light_attack;
+			}
+
+			return damage;
+		}
+
+		void attack(int dmg) {
+			health -= dmg;
+		}
+
+		void heal() {
+			health += healingStat;
+			}
+
+		/*void print(std::string text) {
+			std::cout << text;
+			}*/
+
 };
 
-void main() {
+
+int main() {	
+
 	Player_Base player;
 	player.setHealth(20);
+	player.setHealingStat(4);
 	player.setLAttack(2);
 	player.setHAttack(5);
+	
+	Player_Base enemy;
+	enemy.setHealth(15);
+	enemy.setHealingStat(3);
+	enemy.setLAttack(3);
+	enemy.setHAttack(7);
 
+
+	enemy.attack(player.getDamage());
+	player.attack(enemy.getDamage());
+	enemy.attack(player.getDamage());
+	enemy.heal();
+	enemy.attack(player.getDamage());
+	player.attack(enemy.getDamage());
+	player.heal();
+	player.attack(enemy.getDamage());
+
+	std::cout << "Players health is " << player.getHealth() << "\n";
+	std::cout << "Enemys health is " << enemy.getHealth() << "\n";
+
+
+
+	return 0;
 }
